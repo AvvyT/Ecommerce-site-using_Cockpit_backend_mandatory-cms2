7cb8bf7e7937a15bb9562f58efbeab2e3e1b3183
Radoslava Tsankova Todorova <radoslavatsankovatodorova@MacBook-de-Radoslava.local>
2019-09-09 16:32:19 +0200

    bild visas o checked fixat

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,15 @@ const api = `http://192.168.99.100:8080/api/collections/get/damskor`;
 
 function Home() {
     const [articles, updateArticles] = useState([]);
+    const [checked, updateChecked] = useState(false);
+
+    let chekChecked = `?${checked ? "&filter[Lagersaldo]=true" : ""}`;
+
     // const [numberPosts, updateNumberposts] = useState(1);
 
     useEffect(() => {
 
-        axios.get(api)
+        axios.get(`${api}${chekChecked}`)
             .then((response) => {
                 console.log(response.data.entries);
                 //console.log(response.data.total);
@@ -22,7 +26,7 @@ function Home() {
             .catch(error => {
                 console.log(error);
             });
-    }, []);
+    }, [checked]);
 
     //console.log(numberPosts);
 
@@ -35,14 +39,17 @@ function Home() {
             <label>Search <input className='input' placeholder=' Write your shoe ..'></input></label>
             <br></br>
 
-            <label><input type="checkbox"></input> in stock</label>
+            <label><input type="checkbox"
+                onChange={e => updateChecked(e.target.checked)}
+                checked={checked}
+            ></input> in stock</label>
 
 
             <div className='all-page'>
                 {articles.map((article) => (
                     <div key={article._id} className='items'>
                         <h3><Link to={'/article/' + article._id}>{article.Name}</Link></h3>
-                        <div className='slyle-bild'><img alt='' src={article.Bilder.path} width='150px'></img></div>
+                        <div className='slyle-bild'><img alt={article.Name} src={`http://192.168.99.100:8080/${article.Bilder[0].path}`} width='200px'></img></div>
                         <p>{article.Pris} SEK</p>
                         <p style={{ color: 'lightgreen' }}>{article.Lagersaldo} in stock</p>
                     </div>
